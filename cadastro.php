a46ff0a23841b4b4c82ffebd4f342d9aab146d75
DAVID BATISTA VITOR <professordavidvitor@gmail.com>
2025-01-23 21:25:46 -0300

    Criando caixas de texto e de senha

---
+++ cadastro.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pr-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <p>Os dados foram enviados!</p>
+    <p>Aprenda PHP para saber o que fazer com eles</p>
+    <p><link rel="stylesheet" href="form001.html"></a></p>
+    
+</body>
+</html>
